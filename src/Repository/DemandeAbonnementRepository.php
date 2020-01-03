2bc2ecfc58e5fd184295471e3d77058c0c60e844
Youness ARBOUH <youness.arbouh@gmail.com>
2020-01-03 18:15:50 +0100

    -version 1

---
+++ src/Repository/DemandeAbonnementRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\DemandeAbonnement;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method DemandeAbonnement|null find($id, $lockMode = null, $lockVersion = null)
+ * @method DemandeAbonnement|null findOneBy(array $criteria, array $orderBy = null)
+ * @method DemandeAbonnement[]    findAll()
+ * @method DemandeAbonnement[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class DemandeAbonnementRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, DemandeAbonnement::class);
+    }
+
+    // /**
+    //  * @return DemandeAbonnement[] Returns an array of DemandeAbonnement objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('d.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?DemandeAbonnement
+    {
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
